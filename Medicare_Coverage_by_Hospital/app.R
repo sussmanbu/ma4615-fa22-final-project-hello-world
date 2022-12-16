58b71be91512201784a99d20cc667e02fd369b7e
Fangzhou Cheng <chengfz@bu.edu>
2022-12-16 15:52:51 -0500

    asdf

--- Medicare_Coverage_by_Hospital/app.R
+++ Medicare_Coverage_by_Hospital/app.R
@@ -1,5 +1,6 @@
 library(tidyverse) 
 library(shiny) 
+library(rlang)
 
 medicare_data <- read_csv(here::here("Medicare_Data.csv") , 
                           col_types = cols( Avg_Mdcr_Pymt_Amt=col_number(), 
@@ -21,18 +22,41 @@ states <- medicare_data_clean_int %>% pull(state) %>% unique() %>% sort()
 cities <- medicare_data_clean_int %>% pull(city) %>% unique() %>% sort() 
 hospitals <- medicare_data_clean_int %>% pull(hospital) %>% unique() %>% sort() 
 which_count <- c("total_payment")
-DRG <- medicare_data_clean_int %>% pull(DRG_Desc) %>% unique() %>% sort()
+
 ui <- fluidPage(titlePanel("Medicare Coverage"), 
-                selectInput(inputId= "which", label = "Count", choices = which_count), 
-                selectInput(inputId= "state", label = "State", choices = states), 
-                selectInput(inputId= "city", label = "City", choices = cities),
-                selectInput(inputId= "hospital", label = "Hospital", choices = hospitals),
-                plotOutput("distPlot")
-) 
-server <- function(input, output) {
-  output$distPlot <- renderPlot({medicare_data_clean_int %>% 
-      filter(state == input$state, city == input$city, hospital == input$hospital) %>% 
-      ggplot(aes_string(x = input$which, y = "medicare_payment")) + 
+                fluidRow(
+                  column(width = 3, wellPanel(
+                    helpText("Select which hospital(s) you want to look at or compare."),
+                    selectInput(inputId= "which", label = "Count", choices = which_count), 
+                    selectInput(inputId= "state", label = "State", choices = states, multiple = TRUE, selected = "MA"), 
+                    selectizeInput(inputId= "city", label = "City", choices = NULL),
+                    selectizeInput(inputId= "hospital", label = "Hospital", choices = NULL)
+                  )
+                  ),
+                  mainPanel(plotOutput("distPlot")) ))
+              
+
+server <- function(input, output, session){
+  state <- reactive({
+    filter(medicare_data_clean_int, state == input$state)
+  })
+  observeEvent(state(), {
+    choices <- unique(state()$city)
+    updateSelectizeInput(inputId = "city", choices = choices, selected = "Boston", options = list(maxItems = 9999))
+  })
+  city <- reactive({
+    req(input$city)
+    filter(state(), city == input$city)
+    })
+  observeEvent(city(), {
+    choices <- unique(city()$hospital)
+    updateSelectizeInput(inputId = "hospital", choices = choices, selected = "Boston Medical Center Corporation-", options = list(maxItems = 9999))
+  })
+  output$distPlot <- renderPlot({
+    req(input$hospital)
+    city() %>% 
+      filter(hospital == input$hospital) %>% 
+      ggplot(aes_string(x = input$which, y = "medicare_payment", color = "hospital")) + 
       geom_point() + 
       geom_smooth() + 
       scale_y_log10() +
@@ -40,6 +64,5 @@ server <- function(input, output) {
   })
 }
 shinyApp(ui = ui, server = server)
-
 
 
